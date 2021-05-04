a2bfe041699a38ba98eec6abc44244c51134e4e5
shirne <shirne@126.com>
2021-05-04 12:41:23 +0800

    初始化多语言

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'generated/l10n.dart';
 import 'widgets/game_wrapper.dart';
 import 'game_board.dart';
 
@@ -11,10 +13,20 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Chinese Chess',
+        title: '',
         onGenerateTitle: (BuildContext context){
-          return 'Chinese Chess';
+          return S.of(context).app_title;
         },
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        S.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('zh', 'CN'),
+      ],
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
