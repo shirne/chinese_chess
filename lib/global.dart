90b9275086c5010950b79386535c60ae6507d0ba
shirne <shirne@126.com>
2023-05-08 11:11:21 +0800

    improve log

--- lib/global.dart
+++ lib/global.dart
@@ -1,18 +1,55 @@
-import 'dart:developer';
+import 'dart:developer' as dev;
 
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 export 'utils/core.dart';
 export 'theme.dart';
 
-final logger = Logger.root
+final logger = Logger('LOG')
   ..onRecord.listen((record) {
-    log(
-      record.message,
-      time: record.time,
-      level: record.level.value,
-      error: record.error,
-      stackTrace: record.stackTrace,
-      sequenceNumber: record.sequenceNumber,
-    );
+    if (kDebugMode && record.error != null) {
+      FlutterError.presentError(
+        FlutterErrorDetails(
+          exception: record.error!,
+          stack: record.stackTrace,
+        ),
+      );
+    } else {
+      dev.log(
+        record.message,
+        name: '${record.loggerName} ${levelMoji(record.level)}',
+        time: record.time,
+        level: record.level.value,
+        error: record.error,
+        stackTrace: record.stackTrace,
+        sequenceNumber: record.sequenceNumber,
+      );
+    }
   });
+
+String levelMoji(Level lv) {
+  switch (lv.value) {
+    case 0:
+      break;
+    case 300: //Level.FINEST:
+      return '🎉';
+    case 400: //Level.FINER:
+      return '✨';
+    case 500: //Level.FINE:
+      return '✅';
+    case 700: //Level.CONFIG:
+      return '🔧';
+    case 800: //Level.INFO:
+      return '💡';
+    case 900: //Level.WARNING:
+      return '⚠️';
+    case 1000: //Level.SEVERE:
+      return '❌';
+    case 1200: //Level.SHOUT:
+      return '💥';
+    case 2000: //Level.OFF:
+      return '⛔';
+  }
+  return '♻️';
+}
